1635e7d763b464663c6d88747d19662061200065
Ehren Murdick <emurdick@pivotal.io>
2017-01-24 09:44:30 -0800

    render all votes in line

--- src/components/voting.jsx
+++ src/components/voting.jsx
@@ -6,20 +6,28 @@ const option = (n) => (
   <option key={n} value={n}>{n}</option>
 )
 
+const voteLi = (v) => (
+  <li>{v.points}</li>
+)
+
 const values = [0, 1, 2, 3, 5, 8]
 
 let pointInput
 
-const view = ({vote}) => (
+const view = ({vote, votes}) => (
   <div>
     <select ref={el => pointInput = el}>
       {_.map(values, option)}
     </select>
     <button onClick={vote}>Vote</button>
+    <ul>
+      {_.map(votes, voteLi)}
+    </ul>
   </div>
 )
 
 const mapToProps = (state) => ({
+  votes: state.votes
 })
 
 const mapToDispatch = (dispatch) => ({
