71a4dd277b17349b0d1ad7d5ce871dad37bfba4c
Ehren Murdick <emurdick@pivotal.io>
2017-01-27 14:00:30 -0800

    re-join the current party on page load
    - we have a new websocket, need to tell the server who we are

--- src/actions/joinParty.js
+++ src/actions/joinParty.js
@@ -2,9 +2,9 @@ import { routes } from '../routes'
 
 export const JoinPartyAction = 'JOIN_PARTY'
 
-export const JoinParty = (uuid, userName) => ({
+export const JoinParty = (uuid, userName, redirect = routes.votePath) => ({
   type: JoinPartyAction,
-  nextLocation: routes.votePath,
+  nextLocation: redirect,
   userName,
   uuid,
 })
--- src/middlewares/websocketMiddleware.js
+++ src/middlewares/websocketMiddleware.js
@@ -3,12 +3,19 @@ import { hashHistory } from 'react-router'
 
 import { SyncVote, VoteAction } from '../actions/vote'
 import { CreatePartyAction } from '../actions/createParty'
-import { JoinPartyAction } from '../actions/joinParty'
+import { JoinParty, JoinPartyAction } from '../actions/joinParty'
 import { LeavePartyAction } from '../actions/leaveParty'
 
 let connection = new WebSocket('ws://localhost:8001')
 
 export const websocketMiddleware = store => {
+  connection.onopen = () => {
+    let state = store.getState()
+    if (!_.isNil(state.party.uuid)) {
+      store.dispatch(JoinParty(state.party.uuid, state.currentUser.userName, null))
+    }
+  }
+
   connection.onmessage = (response) => {
     let action = JSON.parse(response.data)
     store.dispatch(action)
